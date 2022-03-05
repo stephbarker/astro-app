9c153c3ede92839051e25172708c2da2cf24e647
Steph Barker <sbarkerdrums@gmail.com>
2022-03-04 20:12:33 -0800

    adds horoscope data to project

---
+++ data.js
@@ -0,0 +1,79 @@
+const horoscopeList = [
+    {
+        id: 0,
+        img: require('./imgs/aries.jpg'),
+        sign: "Aries",
+        daily: "It's seldom about the big things. It's the seemingly small things like how they get your specific brand of humour and laugh at all of your jokes. It's about how they make you feel seen and heard in a way nobody else has before. Today, you're being asked to celebrate the interpersonal relationships that are an eternal source of joy in your world. Some of you may be entering into a partnership of the professional/creative front as well. This is going to be significant for you. Just ensure you clarify the terms and conditions before you begin.",
+    },
+    {
+        id: 1,
+        img: require('./imgs/taurus.jpg'),
+        sign: "Taurus",
+        daily: "Here's the thing, honey: the Universe is *not* a fan of mixed signals. What you are doing by going back and forth on this decision is delaying the manifestation process. So come into a space of clarity about what it is that you want to call in and send your intentions out accordingly. Remember, the Universe is your co-conspirator on this journey and wants you to create a magnificent life for yourself.",
+    },
+    {
+        id: 2,
+        img: require('./imgs/gemini.jpg'),
+        sign: "Gemini",
+        daily: "If not now, then when? We repeat: if not now, then when? The cards are pointing towards a new path. A journey of a lifetime. A journey unlike any other journey that you've undertaken in the past. It's time to trust where your inner compass is pointing towards and take that first step. Remember, it's okay to not have a roadmap. Nobody does. Trust yourself to figure it all out as you go along.",
+    },
+    {
+        id: 3,
+        img: require('./imgs/cancer.jpg'),
+        sign: "Cancer",
+        daily: "The cards are talking about a new job offer. Something that you've been calling into your Universe for a while now. Here's the catch: saying yes to it will require you to make a major life change. Relocating to a new place, for instance, and leaving your old life behind. But, if it helps you rediscover the meaning of freedom and become the version of you you've always wanted to be, it's worth a shot! Besides, you don't want to live a life of 'what if's', do you now?",
+    },
+    {
+        id: 4,
+        img: require('./imgs/leo.jpg'),
+        sign: "Leo",
+        daily: "There are times when we feel like we're walking through the darkness. We don't know what we're doing or what we're being led towards. But, now is *not* such a time in your life, Leo. You are where you are supposed to and you are witnessing steady progress in the right direction. So, keep putting in the work. The cards are also speaking about a creative collaboration that will help take your work to the next level. So, if and when the opportunity to sync your forces arises, just 'yes'.",
+    },
+    {
+        id: 5,
+        img: require('./imgs/virgo.jpg'),
+        sign: "Virgo",
+        daily: "You’ve planted your seeds with loving intention, Virgo. It’s now time to move to the next level. To take inspired action and focus on the many details required to make this project a success. Yes, your rebel breaker will try to get a hold of the reins every once in a while. Make a conscious effort to honour the system (for now) and remind yourself that learning the rules is the first step to unlearning them.",
+    },
+    {
+        id: 6,
+        img: require('./imgs/libra.jpg'),
+        sign: "Libra",
+        daily: "It’s all a dream within a dream within a dream, baby! So, why hold onto the sorrow and the misery? Why play the blame game to a point where it drags you down to the pits of hell? Today, the cards are urging you to breathe, to step into the role of the observer and to recognise the transient nature of everything. 'Dear Universe, Grant me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know the difference.' Repeating the Serenity Prayer will give you the strength you need at this time.",
+    },
+    {
+        id: 7,
+        img: require('./imgs/scorpio.jpg'),
+        sign: "Scorpio",
+        daily: "Every now and then, you have to burst your own bubbles, Scorpio. You have to confront your ego and the limitations that keep you from realising the potential of what you can create. So, step out of your comfort zone. Make friends with the discomfort as you do. This is a time of change. Inner change. The kind that will help you to break out of the cocoon and emerge as the butterfly you're meant to be.",
+    },
+    {
+        id: 8,
+        img: require('./imgs/sagittarius.jpg'),
+        sign: "Sagittarius",
+        daily: "You're not meant to have all the answers, Sagittarius. You're not meant to know where the road is leading you towards right now. Make peace with the discomfort. Make friends with the unknown. Oh, and one more thing! Release the need to make things happen on your 'timeline'. Things will fall into place exactly when they're meant to.",
+    },
+    {
+        id: 9,
+        img: require('./imgs/capricorn.jpg'),
+        sign: "Capricorn",
+        daily: "You are not broken and you don't need to be fixed. You simply need to observe the shadows, the 'imperfections', and all the things you perceive as flaws. Something magical happens when we stop running away from the parts we have been shamed for. Something magical happens when we find a way to embrace it all. This is when the story changes, Capricorn.",
+    },
+    {
+        id: 10,
+        img: require('./imgs/aquarius.jpg'),
+        sign: "Aquarius",
+        daily: "If you were to title this chapter of your life, what would you call it? We propose the following names, Aquarius: “Unleash'', ''Unbecome'', and “Untethered”. You are at a point in your journey where you are bold enough to be yourself and smart enough to know that the opinions of others don't matter. So, embrace your authentic nature, and fiercely so. Live your truth in the most unapologetic manner. By doing so, you will inspire others to break past their self-constructed prisons.",
+    },
+    {
+        id: 11,
+        img: require('./imgs/pisces.jpg'),
+        sign: "Pisces",
+        daily: "Windfalls are cool, Pisces, but, you know what’s cooler? A steady flow of income. The good thing is, you're manifesting the latter for yourself *and* experiencing the stability you have been working towards for some time now. As long as you are consistent in your efforts, you will witness a definite and positive change on the finance front.",
+    },
+    
+];
+
+
+export default horoscopeList;
+
